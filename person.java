6507b8c54f719690805e623ef7223d187a4a05a6
Olivia Peck <olivia_peck@putnam.com>
2019-07-11 15:10:32 -0400

    added state fields and default constructor

--- person.java
+++ person.java
@@ -1,4 +1,13 @@
+import java.util.UUID;
+
 public class Person extends Object {
 // Person object for all types of peoples
 
+private UUID id;
+private String firstName;
+private String lastName;
+
+Person(){
+    this.id = UUID.randomUUID();
+}
 }
